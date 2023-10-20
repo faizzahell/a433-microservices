d4c4ef020f95255dd63241fda7ea945ccb7d96fa
Faizahel <macbookpro@FaizahelJoasa.local>
2023-10-20 22:32:36 +0700

    Adding the build_push_image.sh file to build and push the image and Dockerfile

---
+++ build_push_image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Membangun Docker Image
+docker build -t ghcr.io/faizzahell/order-service:latest .
+
+# Masuk ke GitHub Container Registry
+echo $GH_PACKAGES_TOKEN | docker login ghcr.io -u faizzahell --password-stdin
+
+# Unggah Docker Image ke GitHub Container Registry
+docker push ghcr.io/faizzahell/order-service:latest
+
